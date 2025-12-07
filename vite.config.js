505227dc78fbe209a49bfc57c3e75c94fd4109d5
contractmateai <contractmateai@gmail.com>
2025-12-07 17:30:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        analysis: "analysis.html",
+        contact: "contact.html",
+        privacy: "privacy.html",
+        terms: "terms.html",
+        cookies: "cookies.html"
+      }
+    }
   }
 });
