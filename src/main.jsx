349dba80729e105317ee67b27469f3bb7cbef1d2
contractmateai <contractmateai@gmail.com>
2025-12-07 17:33:40 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";        // 👈 important: .jsx
-import "./styles/home.css";        // 👈 your big CSS file
+import App from "./App.jsx";
+import "./styles/home.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
