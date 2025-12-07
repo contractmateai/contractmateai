c158f555e3d34de39b580ba44dd1ee607a193c57
contractmateai <contractmateai@gmail.com>
2025-12-07 15:56:17 +0100

    Create logostrip.jsx

---
+++ src/logostrip.jsx
@@ -0,0 +1,12 @@
+// src/components/LogoStrip.jsx
+import React from "react";
+
+export default function LogoStrip() {
+  return (
+    <section className="logo-strip">
+      <div className="logo-track" id="logoTrack"></div>
+      <div className="logo-fade left"></div>
+      <div className="logo-fade right"></div>
+    </section>
+  );
+}
