3de19fb7c86bd3e37e44941c6b580246a949a0f6
contractmateai <you@example.com>
2025-12-24 16:12:43 +0100

    Convert contact.html to React and remove legacy HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import Home from "./pages/Home.jsx"; // Or your main page/component
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Contact from "./pages/Contact.jsx";
+import Analysis from "./pages/Analysis.jsx";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/analysis" element={<Analysis />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
