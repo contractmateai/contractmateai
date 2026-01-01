023d5d8ec409ab08773307aa0718e082eaa3a4d2
contractmateai <you@example.com>
2026-01-01 22:53:03 +0100

    Restore original analysis

--- src/components/AnalysisSidebar.jsx
+++ src/components/AnalysisSidebar.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const AnalysisSidebar = () => (
   <aside className="sidebar">
     <div className="brand">
-      <a href="/index.html" aria-label="Go to homepage">
+      <a href="/" aria-label="Go to homepage">
         <img src="https://imgur.com/Z5hv7K9.png" alt="SignSense logo" />
         <span>SignSense</span>
       </a>
     </div>
     <nav className="nav">
       <a className="active" href="#"><img className="arr" src="https://imgur.com/CHGomYz.png" alt="" /><img className="icon" src="https://imgur.com/lnQTDuo.png" alt="" /><span>Overview</span></a>
-      <a href="/index.html"><img className="arr" src="https://imgur.com/xhkeG0y.png" alt="" /><img className="icon" src="https://imgur.com/kE3okdE.png" alt="" /><span>Home</span></a>
-      <a href="/privacy.html"><img className="arr" src="https://imgur.com/xhkeG0y.png" alt="" /><img className="icon" src="https://imgur.com/krMnFVT.png" alt="" /><span>Privacy</span></a>
-      <a href="#"><img className="arr" src="https://imgur.com/xhkeG0y.png" alt="" /><img className="icon" src="https://imgur.com/6zHviT6.png" alt="" /><span>Contact Us</span></a>
-      <a href="#" id="leaveReview2"><img className="arr" src="https://imgur.com/xhkeG0y.png" alt="" /><img className="icon" src="https://imgur.com/zwr9SOe.png" alt="" /><span>Add a Review</span></a>
+      <a href="/"><img className="arr" src="https://imgur.com/xhkeG0y.png" alt="" /><img className="icon" src="https://imgur.com/kE3okdE.png" alt="" /><span>Home</span></a>
+      <a href="/privacy"><img className="arr" src="https://imgur.com/xhkeG0y.png" alt="" /><img className="icon" src="https://imgur.com/krMnFVT.png" alt="" /><span>Privacy</span></a>
+      <a href="/contact"><img className="arr" src="https://imgur.com/xhkeG0y.png" alt="" /><img className="icon" src="https://imgur.com/6zHviT6.png" alt="" /><span>Contact Us</span></a>
+      <a href="https://tally.so/r/3EGJpA" target="_blank" rel="noopener" id="leaveReview2"><img className="arr" src="https://imgur.com/xhkeG0y.png" alt="" /><img className="icon" src="https://imgur.com/zwr9SOe.png" alt="" /><span>Add a Review</span></a>
     </nav>
     <div className="push"></div>
     <div className="end-only" id="sidebarEnd">
